cef4c3a1f35069f4db1e58a996053765ee483f1d
Keshav Kaushik <38274745+kaukeshav@users.noreply.github.com>
2024-11-19 17:04:23 +0530

    Array carry forward pre-problems

---
+++ src/ArrayCountNumOfPairs_Problem.js
@@ -0,0 +1,32 @@
+
+
+/**
+ * Count numbers of pairs in character array where 
+ * arr[i] = a && arr[j] = b && i < j
+ * @param {*} arr 
+ * @returns 
+ */
+function countNumberOfPairs(arr, a, b) {
+   //pair should ['a', 'b'] for this example
+    let numOfPairs = 0;
+    const indexes = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === a) {
+            for(let startIdx = i + 1; startIdx < arr.length; startIdx++) {
+                if (arr[startIdx] === b) {
+                    numOfPairs++;
+                    indexes.push([i, startIdx]);
+                }
+            }
+        }
+    }
+
+    return {
+        numOfPairs, 
+        indexes
+    };
+}
+// Run this to count
+// const arr = ['a', 'c', 'g', 'd', 'g', 'a', 'g'];
+// countNumberOfPairs(arr, 'a', 'b')
